f0e68512efb237896551da1b75fb943caad965cb
Eliminater74 <eliminater74@gmail.com>
2021-07-09 15:44:30 -0400

    Add tcsr200722 Script

---
+++ scripts/tcsr200722.sh
@@ -0,0 +1,391 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 By Eliminater74
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/tcsr200722/openwrt-luci-app
+# File name: tcsr200722.sh
+# Description: OpenWRT Packages by tcsr200722 (Before Update feeds)
+#
+# Updated By Eliminater74
+################################################################################
+
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/Adguardhome
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/ChinaDNS
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/GoQuiet
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/UnblockNeteaseMusic
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/alsa-utils
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/amule
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/annie
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/antfs-mount
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/antileech
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/aria2
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/ariang
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/atinout
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/autocore
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/automount
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/autosamba
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/baidupcs-web
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/bash
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/bc
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/bcm27xx-eeprom
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/boost
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/bpytop
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/brcmfmac4366c0-firmware-vendor
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/brook
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/btrfs-progs
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cgroupfs-mount
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/chinadns-ng
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cifs-utils
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/compressed-memory
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/containerd
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/coreutils
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cpp-httplib
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cpulimit-ng
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cpulimit
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/csstidy
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/cxxopts
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/ddns-scripts
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/default-settings
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dns-forwarder
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dns2socks
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dnscrypt-proxy2
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dnsforwarder
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dnsmasq-china-list
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/docker
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dockerd
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dsmboot
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/duktape
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/dvb
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/etherwake
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/expat
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/fast-classifier
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/fcgi
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/ffmpeg
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/filebrowser
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/filerun
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/forked-daapd
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/frp
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/fullconenat
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/glib2
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/gmediarender
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/gnunet
+svn co https://github.com/tcsr200722/openwrt-luci-app/trunk/gost
+gotop
+gowebdav
+gstreamer1
+haproxy
+httping
+https-dns-proxy
+i2c-tools
+imagemagick
+ipset-lists
+ipt2socks-alt
+ipt2socks
+iptvhelper
+jpcre2
+k3-brcmfmac4366c-firmware
+k3screenctrl
+kcptun
+libcap
+libcryptopp
+libcups
+libev
+libgcrypt
+libgd
+libmaxminddb
+libnetwork
+libpam
+libpng
+libseccomp
+libssh2
+libtar
+libtorrent-rasterbar
+libunistring
+libupnp
+lm-sensors
+lua-cjson
+lua-maxminddb
+lua-md5
+luasec
+luasocket
+luci-app-accesscontrol
+luci-app-adguardhome
+luci-app-advanced
+luci-app-airplay2
+luci-app-airwhu
+luci-app-aliddns
+luci-app-amule
+luci-app-argon-config
+luci-app-arpbind
+luci-app-atinout
+luci-app-autoipsetadder
+luci-app-autopoweroff
+luci-app-autoreboot
+luci-app-autorepeater
+luci-app-baidupcs-web
+luci-app-beamInDocker
+luci-app-beardropper
+luci-app-brook-server
+luci-app-brook
+luci-app-bypass
+luci-app-cd8021x
+luci-app-chinadns-ng
+luci-app-chinadns
+luci-app-cifs-mount
+luci-app-cifs
+luci-app-cifsd
+luci-app-clash
+luci-app-compressed-memory
+luci-app-control-timewol
+luci-app-control-webrestriction
+luci-app-control-weburl
+luci-app-cpufreq
+luci-app-cpulimit
+luci-app-diskman
+luci-app-dnscrypt-proxy2
+luci-app-dnsfilter
+luci-app-dnsforwarder
+luci-app-dnsmasq-ipset
+luci-app-docker
+luci-app-dockerman
+luci-app-eqos
+luci-app-familycloud
+luci-app-fileassistant
+luci-app-filebrowser
+luci-app-filerun
+luci-app-filetransfer
+luci-app-frpc
+luci-app-frps
+luci-app-godproxy
+luci-app-gost
+luci-app-gowebdav
+luci-app-guest-wifi
+luci-app-haproxy-tcp
+luci-app-homebridge
+luci-app-ipsec-server
+luci-app-ipsec-vpnd
+luci-app-ipsec-vpnserver-manyusers
+luci-app-iptvhelper
+luci-app-jd-dailybonus
+luci-app-k3screenctrl
+luci-app-k3usb
+luci-app-kcptun
+luci-app-kodexplorer
+luci-app-mentohust
+luci-app-minieap
+luci-app-mtwifi
+luci-app-music-remote-center
+luci-app-mwan3helper
+luci-app-mwol
+luci-app-n2n_v2
+luci-app-naiveproxy
+luci-app-netdata
+luci-app-netkeeper-interception
+luci-app-nextdns
+luci-app-nfs
+luci-app-nft-qos
+luci-app-nginx-pingos
+luci-app-ngrokc
+luci-app-njitclient
+luci-app-nodogsplash
+luci-app-nps
+luci-app-oaf
+luci-app-oled
+luci-app-onliner
+luci-app-openclash
+luci-app-openvpn-server
+luci-app-oscam
+luci-app-passwall
+luci-app-pcap-dnsproxy
+luci-app-phtunnel
+luci-app-poweroff
+luci-app-pppoe-relay
+luci-app-pppoe-server
+luci-app-pptp-server
+luci-app-ps3netsrv
+luci-app-qbittorrent
+luci-app-ramfree
+luci-app-rclone
+luci-app-samba4
+luci-app-scutclient
+luci-app-serverchan
+luci-app-smartdns
+luci-app-smstool
+luci-app-socat
+luci-app-softethervpn5
+luci-app-speederv2
+luci-app-ssocks
+luci-app-ssr-libev-server
+luci-app-ssr-mudb-server
+luci-app-ssr-plus
+luci-app-ssrserver-python
+luci-app-syncdial
+luci-app-syncthing
+luci-app-sysuh3c
+luci-app-tcpdump
+luci-app-tencentddns
+luci-app-terminal
+luci-app-timecontrol
+luci-app-timewol
+luci-app-trojan-server
+luci-app-ttnode
+luci-app-ttyd
+luci-app-turboacc
+luci-app-udp2raw
+luci-app-unblockmusic
+luci-app-upnp
+luci-app-usb-printer
+luci-app-usb3disable
+luci-app-uugamebooster
+luci-app-v2ray-server
+luci-app-verysync
+luci-app-vlmcsd
+luci-app-vsftpd
+luci-app-vssr
+luci-app-webadmin
+luci-app-webrestriction
+luci-app-weburl
+luci-app-wifidog
+luci-app-wrtbwmon
+luci-app-xlnetacc
+luci-app-xunlei
+luci-app-zerotier
+luci-lib-docker
+luci-lib-fs
+luci-proto-bonding
+luci-proto-minieap
+luci-proto-netkeeper
+luci-theme-argon
+luci-theme-atmaterial
+luci-theme-bootstrap-blue
+luci-theme-bootstrap-mod
+luci-theme-edge
+luci-theme-infinityfreedom
+luci-theme-netgear
+luci-theme-opentomato
+luci-theme-opentomcat
+luci-theme-opentopd
+mentohust
+microsocks
+microsocks11
+minieap
+miniupnpd
+msgpack-c
+mt-drivers
+mt7601u-ap
+mtk-eip93
+mwan3
+mwol
+n2n_v2
+naiveproxy
+ndpi-netfilter
+net-snmp
+netdata
+netkeeper-interception
+netkeeper
+nextdns
+nfs-kernel-server
+nft-qos
+nginx
+ngrokc
+njitclient
+node-request
+node
+nps
+ntfs-3g
+oaf
+ootoc
+open-app-filter
+open-vm-tools
+openldap
+openssl1.1
+openvpn-easy-rsa-whisky
+openvpn
+openwrt-Pcap_DNSProxy
+openwrt-dnsforwarder
+openwrt-pdnsd-full
+openwrt-shadowvpn-master
+openwrt-udp2raw
+oscam
+p7zip
+pandownload-fake-server
+parted
+pcre2
+pdnsd-alt
+phicomm-k3screenctrl
+php7
+phtunnel
+polarssl
+proxychains-ng
+ps3netsrv
+python3
+qBittorrent-Enhanced-Edition
+qBittorrent
+quickjs
+r8125
+r8152
+r8168
+rapidjson
+rblibtorrent
+rclone-ng
+rclone-webui-react
+rclone
+redsocks2
+rtl8192du
+rtl8812au-ac
+rtl8821cu
+rtl88x2bu
+ruby
+runc
+samba4
+scutclient
+shadowsocks-libev
+shadowsocksr-libev
+shairport-sync
+shellsync
+shortcut-fe
+simple-obfs
+simple-torrent
+smartdns
+smartmontools
+softethervpn5
+spawn-fcgi
+sqm-scripts
+srelay
+ssocks
+strongswan
+sub-web
+subconverter
+subweb
+sysuh3c
+tcping-simple
+tcping
+tcpping
+tini
+tinyfecvpn
+tinyportmapper
+tmate
+trojan-go
+trojan-plus
+trojan
+ttyd
+udpspeeder
+unzip
+uugamebooster
+v2ray-plugin
+v2ray
+verysync
+vlmcsd
+vsftpd-alt
+vssr
+webdav-go
+wget
+wol
+wrtbwmon
+wsdd2
+wxbase
+xray-core
+zoneinfo
