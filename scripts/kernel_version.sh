b6dd593a66d9323341db600b3fb073f369c3200b
Eliminater74 <eliminater74@gmail.com>
2021-07-11 17:47:30 -0400

    Format kernel_version.sh

--- scripts/kernel_version.sh
+++ scripts/kernel_version.sh
@@ -10,22 +10,21 @@
 #
 #################################################################
 
-
 cd openwrt
-find build_dir/ -name .vermagic -exec cat {} \; > VERMAGIC # Find hash
-find build_dir/ -name "linux-*.*.*" -type d > KERNELVERSION # find kernel version
-lineA=$(head -n 1 KERNELVERSION) # Read kernel version from file
-lineB=$(head -n 1 VERMAGIC) # read kernel hash from file
-lineC="${lineA: -13}" # Get last 13 chars from kernel version
-lineD="${lineA: -7}" # Get last 7 chars from kernel version
-rm -rf VERMAGIC KERNELVERSION # remove both files, Not needed anymore
+find build_dir/ -name .vermagic -exec cat {} \; >VERMAGIC  # Find hash
+find build_dir/ -name "linux-*.*.*" -type d >KERNELVERSION # find kernel version
+lineA=$(head -n 1 KERNELVERSION)                           # Read kernel version from file
+lineB=$(head -n 1 VERMAGIC)                                # read kernel hash from file
+lineC="${lineA: -13}"                                      # Get last 13 chars from kernel version
+lineD="${lineA: -7}"                                       # Get last 7 chars from kernel version
+rm -rf VERMAGIC KERNELVERSION                              # remove both files, Not needed anymore
 cd bin/targets/*/*
-echo "TARGET_DIR=$PWD" >> $GITHUB_ENV
+echo "TARGET_DIR=$PWD" >>$GITHUB_ENV
 TARGET_DIR=$PWD
-KERNEL_VER=$lineC"-"$lineB # add together to complete
-KMOD_DIR=$lineD"-"$lineB # add together to complete
-echo "KERNEL_VER=$lineC"-"$lineB" >> $GITHUB_ENV # store in get actions
-echo "KMOD_DIR=$lineD"-"$lineB" >> $GITHUB_ENV # store in get actions
+KERNEL_VER=$lineC"-"$lineB                      # add together to complete
+KMOD_DIR=$lineD"-"$lineB                        # add together to complete
+echo "KERNEL_VER=$lineC"-"$lineB" >>$GITHUB_ENV # store in get actions
+echo "KMOD_DIR=$lineD"-"$lineB" >>$GITHUB_ENV   # store in get actions
 echo "------------------------------------------------"
 echo "Kernel: $KERNEL_VER" # testing
 echo "DIR: $KMOD_DIR"
@@ -42,14 +41,4 @@ cd $TARGET_DIR
 rm -rf $TARGET_DIR/kmods # Remove kmods folder
 cd ${GITHUB_WORKSPACE}/openwrt
 
-echo "--------------------------------------"
-echo "CMD: $ARCH_PACKAGES"
-echo "CMD: $BOARD"
-echo "CMD: $SUBTARGET"
-echo "CMD: $TARGET_OPTIMIZATION"
-echo "CMD: $TARGET_SUFFIX"
-echo "CMD: $BUILD_SUFFIX"
-echo "CMD: $SUBDIR"
-echo "CMD: $BUILD_SUBDIR"
-echo "--------------------------------------"
 exit 0
