955e68a7709a0e7fb20490c5d1a3abb1d6c97062
Eliminater74 <eliminater74@gmail.com>
2021-07-18 18:42:36 -0400

    Add New DevOpenWRT-Router.sh So that my personal fixes are now loaded external.

---
+++ scripts/DevOpenWRT-Router.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 By Eliminater74
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/DevOpenWRT-Router?type=source
+# File name: DevOpenWRT-Router.sh
+# Description: OpenWRT Packages by Eliminater74
+#
+# Updated By Eliminater74
+################################################################################
+
+### luci-app-log
+git clone https://github.com/DevOpenWRT-Router/luci-app-log.git package/luci-app-log
+### luci-app-tn-logview
+git clone https://github.com/DevOpenWRT-Router/luci-app-tn-logview.git package/luci-app-tn-logview
+### luci-app-tn-ttyd
+git clone https://github.com/DevOpenWRT-Router/luci-app-tn-ttyd.git package/luci-app-tn-ttyd
+### luci-app-tn-shellinabox
+git clone https://github.com/DevOpenWRT-Router/luci-app-tn-shellinabox.git package/luci-app-tn-shellinabox
+### luci-app-tn-watchdog
+git clone https://github.com/DevOpenWRT-Router/luci-app-tn-watchdog.git package/luci-app-tn-watchdog
+### luci-app-tn-netports
+git clone https://github.com/DevOpenWRT-Router/luci-app-tn-netports.git package/luci-app-tn-netports
+### luci-app-interfaces-statistics
+git clone https://github.com/DevOpenWRT-Router/luci-app-interfaces-statistics.git package/luci-app-interfaces-statistics
+### luci-app-cpu-status
+git clone https://github.com/DevOpenWRT-Router/luci-app-cpu-status.git package/luci-app-cpu-status
+### luci-app-temp-status
+git clone https://github.com/DevOpenWRT-Router/luci-app-temp-status.git package/luci-app-temp-status
+### luci-app-rebootschedule
+git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule.git package/luci-app-rebootschedule
+### luci-app-timecontrol
+git clone https://github.com/DevOpenWRT-Router/luci-app-timecontrol.git package/luci-app-timecontrol
+### luci-app-cpulimit
+git clone https://github.com/DevOpenWRT-Router/luci-app-cpulimit.git package/luci-app-cpulimit
+### luci-app-mqos
+git clone https://github.com/DevOpenWRT-Router/luci-app-mqos.git package/luci-app-mqos
+### luci-app-disks-info
+git clone https://github.com/DevOpenWRT-Router/luci-app-disks-info.git package/luci-app-disks-info
+### NetSpeedTest
+git clone https://github.com/DevOpenWRT-Router/netspeedtest.git package/NetSpeedTest
+### luci-app-netdata
+git clone https://github.com/DevOpenWRT-Router/luci-app-netdata.git package/luci-app-netdata
+
+
+
+
+echo "End of DevOpenWRT-Router.sh"
+exit 0
--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,39 +15,6 @@
 ### Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-
-### luci-app-log
-git clone https://github.com/DevOpenWRT-Router/luci-app-log.git package/luci-app-log
-### luci-app-tn-logview
-git clone https://github.com/DevOpenWRT-Router/luci-app-tn-logview.git package/luci-app-tn-logview
-### luci-app-tn-ttyd
-git clone https://github.com/DevOpenWRT-Router/luci-app-tn-ttyd.git package/luci-app-tn-ttyd
-### luci-app-tn-shellinabox
-git clone https://github.com/DevOpenWRT-Router/luci-app-tn-shellinabox.git package/luci-app-tn-shellinabox
-### luci-app-tn-watchdog
-git clone https://github.com/DevOpenWRT-Router/luci-app-tn-watchdog.git package/luci-app-tn-watchdog
-### luci-app-tn-netports
-git clone https://github.com/DevOpenWRT-Router/luci-app-tn-netports.git package/luci-app-tn-netports
-### luci-app-interfaces-statistics
-git clone https://github.com/DevOpenWRT-Router/luci-app-interfaces-statistics.git package/luci-app-interfaces-statistics
-### luci-app-cpu-status
-git clone https://github.com/DevOpenWRT-Router/luci-app-cpu-status.git package/luci-app-cpu-status
-### luci-app-temp-status
-git clone https://github.com/DevOpenWRT-Router/luci-app-temp-status.git package/luci-app-temp-status
-### luci-app-rebootschedule
-git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule.git package/luci-app-rebootschedule
-### luci-app-timecontrol
-git clone https://github.com/DevOpenWRT-Router/luci-app-timecontrol.git package/luci-app-timecontrol
-### luci-app-cpulimit
-git clone https://github.com/DevOpenWRT-Router/luci-app-cpulimit.git package/luci-app-cpulimit
-### luci-app-mqos
-git clone https://github.com/DevOpenWRT-Router/luci-app-mqos.git package/luci-app-mqos
-### luci-app-disks-info
-git clone https://github.com/DevOpenWRT-Router/luci-app-disks-info.git package/luci-app-disks-info
-### NetSpeedTest
-git clone https://github.com/DevOpenWRT-Router/netspeedtest.git package/NetSpeedTest
-### luci-app-netdata
-git clone https://github.com/DevOpenWRT-Router/luci-app-netdata.git package/luci-app-netdata
 ### luci-app-filebrowser
 git clone https://github.com/xiaozhuai/luci-app-filebrowser.git package/luci-app-filebrowser
 ### luci-app-eqos
