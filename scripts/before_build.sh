e636039c0b13729a53fcd760ec8d0520e1286466
Eliminater74 <eliminater74@gmail.com>
2021-08-13 07:56:20 -0400

    add git revert

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -3,8 +3,8 @@
 # Enter your commands here, e.g.
 # echo "Start build!"
 cd openwrt
-git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
-
+# git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
+git revert --no-edit eaa9c94c75748265b9260691d5f59c9d3711f7d4
 
 
 exit 0
