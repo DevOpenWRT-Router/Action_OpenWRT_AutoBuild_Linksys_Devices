696d98a4b69d21ffaccfcadd243969da53b0f98b
Eliminater74 <eliminater74@gmail.com>
2021-08-14 03:23:40 -0400

    Move Custom LUCI Themes to New LUCI_Themes Script.

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -60,39 +60,7 @@ git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-
 git clone https://github.com/a920025608/luci-app-usbnet.git package/luci-app-usbnet
 git clone https://github.com/a920025608/usbnet.git package/usbnet
 
-### ----------------------------------------------------------------------- ###
-### THEMES ###
-### new argon theme
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-### New argon theme control program
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-### luci-theme-opentomcat
-# git clone https://github.com/chaoxiaozhu/luci-theme-opentomcat.git package/luci-theme-opentomcat
-### luci-theme-rosy
-# git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
-### luci-theme-netgear
-# git clone https://github.com/ysoyipek/luci-theme-netgear.git package/luci-theme-netgear
-### luci-theme-edge2 ###
-# git clone -b main https://github.com/YL2209/luci-theme-edge2.git package/luci-theme-edge2
-### luci-theme-opentopd thme openwrt theme
-# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-### btmod theme
-# git clone https://github.com/sirpdboy/luci-theme-btmod.git package/luci-theme-btmob
-### luci-theme-opentomcat
-# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-### luci-theme-netgear
-# git clone https://github.com/i028/luci-theme-netgear.git package/luci-theme-netgear
-### luci-theme-ifit
-# git clone https://github.com/YL2209/luci-theme-ifit.git package/luci-theme-ifit
-### luci-theme-surfboard
-# git clone https://github.com/SURFBOARD-ONE/luci-theme-surfboard.git package/luci-theme-surfboard
-### luci-theme-atmaterial
-# git clone https://github.com/miccjing/luci-theme-atmaterial.git package/luci-theme-atmaterial
-### luci-theme-mcat
-# git clone https://github.com/fszok/luci-theme-mcat.git package/luci-theme-mcat
-### luci-theme-fate
-# git clone https://github.com/fatelpc/luci-theme-fate.git package/luci-theme-fate
-### ----------------------------------------------------------------------- ###
+
 
 
 echo "End of diy-part1.sh"
