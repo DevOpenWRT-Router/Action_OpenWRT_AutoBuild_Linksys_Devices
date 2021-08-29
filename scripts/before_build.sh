8e8d7dd59ee3c4f82815b3b31bdb2506ff781b4f
Eliminater74 <eliminater74@gmail.com>
2021-08-29 16:32:07 -0400

    update: before_build.sh

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -2,7 +2,7 @@
 
 # Enter your commands here, e.g.
 # echo "Start build!"
-cd openwrt
+cd openwrt || exit
 
 # git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
 # git revert --no-edit eaa9c94c75748265b9260691d5f59c9d3711f7d4
