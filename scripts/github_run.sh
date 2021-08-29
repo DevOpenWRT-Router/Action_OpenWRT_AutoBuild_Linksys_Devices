bd2ff94e9b73ad6a35235e2c59fdafdc0dd37f63
Eliminater74 <eliminater74@gmail.com>
2021-08-29 16:02:07 -0400

    Update: github_run.sh

--- scripts/github_run.sh
+++ scripts/github_run.sh
@@ -12,24 +12,7 @@
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
-SOURCE_DIRECTORY=${{ env.source-directory }}
-DESTINATION_GITHUB_USERNAME=${{ env.destination-github-username }}
-DESTINATION_REPOSITORY_NAME=${{ env.destination-repository-name }}
-USER_EMAIL=${{ env.user-email }}
-USER_NAME=${{ env.user-name }}
-DESTINATION_REPOSITORY_USERNAME=${{ env.destination-repository-username }}
-TARGET_BRANCH=${{ env.target-branch }}
-COMMIT_MESSAGE=${{ env.commit-message }}
-TARGET_DIRECTORY=${{ env.target-directory }}
-
 echo "[+] Test 1"
-echo "[+] $DESTINATION_GITHUB_USERNAME And $DESTINATION_REPOSITORY_NAME Are Listed:"
-
-
-
-
-CLONE_DIR=$(mktemp -d)
-TEMP_DIR=$(mktemp -d)
 
 cd openwrt/bin
 D="$(date +"%Y.%m.%d-%H%M")"
