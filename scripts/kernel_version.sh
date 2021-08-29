4c1aa7c868d8a93b293aade7a249670b7c301937
Eliminater74 <eliminater74@gmail.com>
2021-08-29 16:27:09 -0400

    Update kernel_version.sh

--- scripts/kernel_version.sh
+++ scripts/kernel_version.sh
@@ -10,7 +10,7 @@
 #
 #################################################################
 
-cd openwrt
+cd openwrt || exit
 find build_dir/ -name .vermagic -exec cat {} \; >VERMAGIC  # Find hash
 find build_dir/ -name "linux-5.*.*" -type d >KERNELVERSION # find kernel version
 lineA=$(head -n 1 KERNELVERSION)                           # Read kernel version from file
@@ -18,7 +18,7 @@ lineB=$(head -n 1 VERMAGIC)                                # read kernel hash fr
 lineC="${lineA: -13}"                                      # Get last 13 chars from kernel version
 lineD="${lineA: -7}"                                       # Get last 7 chars from kernel version
 rm -rf VERMAGIC KERNELVERSION                              # remove both files, Not needed anymore
-cd bin/targets/*/*
+cd bin/targets/*/* || exit
 echo "TARGET_DIR=$PWD" >>$GITHUB_ENV
 TARGET_DIR=$PWD
 KERNEL_VER=$lineC"-"$lineB                      # add together to complete
@@ -29,14 +29,14 @@ echo "------------------------------------------------"
 echo "Kernel: $KERNEL_VER" # testing
 echo "DIR: $KMOD_DIR"
 echo "------------------------------------------------"
-cd ${GITHUB_WORKSPACE}/openwrt
+cd ${GITHUB_WORKSPACE}/openwrt || exit
 mkdir -p bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR
 rsync '--include=/kmod-*.ipk' '--exclude=*' -va bin/targets/mvebu/cortexa9/packages/ bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
 make -j32 package/index V=s CONFIG_SIGNED_PACKAGES= PACKAGE_SUBDIRS=bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
-cd bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR
+cd bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR || exit
 tar -cvzf kmods_$KMOD_DIR.tar.gz ./*
 mv kmods_$KMOD_DIR.tar.gz ${GITHUB_WORKSPACE}/openwrt/bin/targets/mvebu/cortexa9/
-cd ${GITHUB_WORKSPACE}/openwrt
+cd ${GITHUB_WORKSPACE}/openwrt || exit
 # rm -rf bin/targets/mvebu/cortexa9/kmods
 # cd ${GITHUB_WORKSPACE}/openwrt
 
