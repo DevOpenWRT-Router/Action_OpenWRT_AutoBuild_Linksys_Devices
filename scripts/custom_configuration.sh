852c75181a2bbe37ee241c0ac55397f825870b9c
Eliminater74 <eliminater74@gmail.com>
2022-03-14 08:57:40 -0400

    Clear out custom_configuration.sh

---
+++ scripts/custom_configuration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 - 2022 By Eliminater74
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+#
+# Updated By Eliminater74 03/14/2022
+################################################################################
+
+
+
+echo "End of custom_configuration.sh"
+exit 0
