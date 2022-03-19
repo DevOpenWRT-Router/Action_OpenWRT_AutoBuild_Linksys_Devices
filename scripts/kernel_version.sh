bf433ad7a43043e7af799a54e88ec545b03895bb
Eliminater74 <eliminater74@gmail.com>
2022-03-18 23:59:19 -0400

    Redo Kernel_version.sh Using Functions this time.

--- scripts/kernel_version.sh
+++ scripts/kernel_version.sh
@@ -10,6 +10,7 @@
 #
 #################################################################
 
+kernel_version() {
 cd openwrt || exit
 find build_dir/ -name .vermagic -exec cat {} \; >VERMAGIC  # Find hash
 find build_dir/ -name "linux-5.*.*" -type d >KERNELVERSION # find kernel version
@@ -27,23 +28,30 @@ echo "------------------------------------------------"
 echo "Kernel: $KERNEL_VER" # testing
 echo "DIR: $KMOD_DIR"
 echo "------------------------------------------------"
+ls
+}
 
 if [ "$1" == "TEST" ]; then
   echo "THIS WORKED LIKE IT SHOULD"
 fi
 
+package_archive() {
+cd ${GITHUB_WORKSPACE}/openwrt || exit
+mkdir -p bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR
+rsync '--include=/kmod-*.ipk' '--exclude=*' -va bin/targets/mvebu/cortexa9/packages/ bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
+make -j32 package/index V=s CONFIG_SIGNED_PACKAGES= PACKAGE_SUBDIRS=bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
+cd bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR || exit
+tar -cvzf kmods_$KMOD_DIR.tar.gz ./*
+mv kmods_$KMOD_DIR.tar.gz ${GITHUB_WORKSPACE}/openwrt/bin/targets/mvebu/cortexa9/
+cd ${GITHUB_WORKSPACE}/openwrt
+}
+#         rm -rf bin/targets/mvebu/cortexa9/kmods
+#         cd ${GITHUB_WORKSPACE}/openwrt
+
 
 #          echo 'src/gz purefusion_kmods https://raw.githubusercontent.com/DevOpenWRT-Router/Linksys_OpenWRT_Releases/main/kmods/$KMOD_DIR' >> openwrt/package/system/opkg/files/customfeeds.conf
 
-#cd ${GITHUB_WORKSPACE}/openwrt || exit
-#mkdir -p bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR
-#rsync '--include=/kmod-*.ipk' '--exclude=*' -va bin/targets/mvebu/cortexa9/packages/ bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
-#make -j32 package/index V=s CONFIG_SIGNED_PACKAGES= PACKAGE_SUBDIRS=bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR/
-#cd bin/targets/mvebu/cortexa9/kmods/$KMOD_DIR || exit
-#tar -cvzf kmods_$KMOD_DIR.tar.gz ./*
-#mv kmods_$KMOD_DIR.tar.gz ${GITHUB_WORKSPACE}/openwrt/bin/targets/mvebu/cortexa9/
-#cd ${GITHUB_WORKSPACE}/openwrt || exit
-## rm -rf bin/targets/mvebu/cortexa9/kmods
-## cd ${GITHUB_WORKSPACE}/openwrt
 
+
+$1
 exit 0
