0b9b5988db1994695e74e275c8d4273198116658
Eliminater74 <eliminater74@gmail.com>
2022-03-24 19:20:18 -0400

    fetch_packages.sh: update

--- scripts/fetch_packages.sh
+++ scripts/fetch_packages.sh
@@ -71,6 +71,7 @@ do
   svn co https://github.com/kenzok8/small-package/trunk/${packages[$i]} package/kenzok8/${packages[$i]}
 done
 
+rm -rf package/kenzok8/default-settings # using a dif
 rm -rf package/kenzok8/.github
 rm -rf package/kenzok8/main.sh
 rm -rf package/kenzok8/LICENSE
@@ -159,34 +160,34 @@ do
   svn co https://github.com/sirpdboy/sirpdboy-package/trunk/${packages[$i]} package/sirpdboy/${packages[$i]}
 done
 
-rm -rf  package/sirpdboy/adguardhome
-rm -rf  package/sirpdboy/doc
-rm -rf  package/sirpdboy/luci-app-access-control ## NEEDS FIXED
-rm -rf  package/sirpdboy/luci-app-baidupcs-web
-rm -rf  package/sirpdboy/luci-app-chinadns-ng
-rm -rf  package/sirpdboy/luci-app-cpulimit
-rm -rf  package/sirpdboy/luci-app-dockerman
-rm -rf  package/sirpdboy/luci-app-easymesh
-rm -rf  package/sirpdboy/luci-app-netdata
-rm -rf  package/sirpdboy/luci-app-netspeedtest
-rm -rf  package/sirpdboy/luci-app-onliner
-rm -rf  package/sirpdboy/luci-app-ramfree
-rm -rf  package/sirpdboy/luci-app-rebootschedule
-rm -rf  package/sirpdboy/luci-app-smartdns
-rm -rf  package/sirpdboy/luci-app-socat
-rm -rf  package/sirpdboy/luci-app-timecontrol
-rm -rf  package/sirpdboy/luci-app-wrtbwmon
-rm -rf  package/sirpdboy/luci-theme-argon_new
-rm -rf  package/sirpdboy/luci-theme-atmaterial
-rm -rf  package/sirpdboy/luci-theme-btmod
-rm -rf  package/sirpdboy/luci-theme-edge
-rm -rf  package/sirpdboy/luci-theme-ifit
-rm -rf  package/sirpdboy/luci-theme-opentomato
-rm -rf  package/sirpdboy/luci-theme-opentomcat
-rm -rf  package/sirpdboy/luci-theme-opentopd
-rm -rf  package/sirpdboy/netdata
-rm -rf  package/sirpdboy/speedtest-cli ## NEEDS FIXED
-rm -rf  package/sirpdboy/smartdns ## NEEDS FIXED
+rm -rf package/sirpdboy/adguardhome
+rm -rf package/sirpdboy/doc
+rm -rf package/sirpdboy/luci-app-access-control ## NEEDS FIXED
+rm -rf package/sirpdboy/luci-app-baidupcs-web
+rm -rf package/sirpdboy/luci-app-chinadns-ng
+rm -rf package/sirpdboy/luci-app-cpulimit
+rm -rf package/sirpdboy/luci-app-dockerman
+rm -rf package/sirpdboy/luci-app-easymesh
+rm -rf package/sirpdboy/luci-app-netdata
+rm -rf package/sirpdboy/luci-app-netspeedtest
+rm -rf package/sirpdboy/luci-app-onliner
+rm -rf package/sirpdboy/luci-app-ramfree
+rm -rf package/sirpdboy/luci-app-rebootschedule
+rm -rf package/sirpdboy/luci-app-smartdns
+rm -rf package/sirpdboy/luci-app-socat
+rm -rf package/sirpdboy/luci-app-timecontrol
+rm -rf package/sirpdboy/luci-app-wrtbwmon
+rm -rf package/sirpdboy/luci-theme-argon_new
+rm -rf package/sirpdboy/luci-theme-atmaterial
+rm -rf package/sirpdboy/luci-theme-btmod
+rm -rf package/sirpdboy/luci-theme-edge
+rm -rf package/sirpdboy/luci-theme-ifit
+rm -rf package/sirpdboy/luci-theme-opentomato
+rm -rf package/sirpdboy/luci-theme-opentomcat
+rm -rf package/sirpdboy/luci-theme-opentopd
+rm -rf package/sirpdboy/netdata
+rm -rf package/sirpdboy/speedtest-cli ## NEEDS FIXED
+rm -rf package/sirpdboy/smartdns ## NEEDS FIXED
 
 echo "END of sirpdboy's packages"
 
@@ -213,24 +214,24 @@ do
 done
 
 #rm -rf  package/sirpdboy/autocore
-rm -rf  package/sirpdboy/automount
-rm -rf  package/sirpdboy/autosamba-samba4
-rm -rf  package/sirpdboy/default-settings # using a dif
-rm -rf  package/sirpdboy/doc # not a package
-rm -rf  package/sirpdboy/gcc # not a package
-rm -rf  package/sirpdboy/ksmbd-tools
-rm -rf  package/sirpdboy/luci-app-ksmbd
-rm -rf  package/sirpdboy/luci-app-samba
-rm -rf  package/sirpdboy/luci-app-samba4
-rm -rf  package/sirpdboy/miniupnpd
-rm -rf  package/sirpdboy/mwan3
-rm -rf  package/sirpdboy/samba36
-rm -rf  package/sirpdboy/samba4
-rm -rf  package/sirpdboy/my-autocore
-rm -rf  package/sirpdboy/mycore
-rm -rf  package/sirpdboy/pass
-rm -rf  package/sirpdboy/set # Not a package
-rm -rf  package/sirpdboy/socat
+rm -rf package/sirpdboy/automount
+rm -rf package/sirpdboy/autosamba-samba4
+rm -rf package/sirpdboy/default-settings # using a dif
+rm -rf package/sirpdboy/doc # not a package
+rm -rf package/sirpdboy/gcc # not a package
+rm -rf package/sirpdboy/ksmbd-tools
+rm -rf package/sirpdboy/luci-app-ksmbd
+rm -rf package/sirpdboy/luci-app-samba
+rm -rf package/sirpdboy/luci-app-samba4
+rm -rf package/sirpdboy/miniupnpd
+rm -rf package/sirpdboy/mwan3
+rm -rf package/sirpdboy/samba36
+rm -rf package/sirpdboy/samba4
+rm -rf package/sirpdboy/my-autocore
+rm -rf package/sirpdboy/mycore
+rm -rf package/sirpdboy/pass
+rm -rf package/sirpdboy/set # Not a package
+rm -rf package/sirpdboy/socat
 
 echo "END of sirpdboy's Build packages"
 }
