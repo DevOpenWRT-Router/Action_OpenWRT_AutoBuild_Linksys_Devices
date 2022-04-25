9e20acd4ed675a0c9c40619e2538d9c216ad7ede
Eliminater74 <eliminater74@gmail.com>
2022-04-25 13:39:25 -0400

    fetch: kenzok8:  remove smartdns

--- scripts/fetch_packages.sh
+++ scripts/fetch_packages.sh
@@ -128,6 +128,8 @@ rm -rf package/kenzok8/my-default-settings # using a dif
 rm -rf package/kenzok8/my-autocore # Using the one above in unsorted
 rm -rf package/kenzok8/mosdns # Build Errors
 rm -rf package/kenzok8/luci-app-mosdns
+rm -rf package/kenzok8/luci-app-smartdns
+rm -rf package/kenzok8/smartdns
 #rm -rf package/kenzok8/luci-app-netspeedtest
 rm -rf package/kenzok8/v2ray-core
 rm -rf package/kenzok8/v2ray-geodata
