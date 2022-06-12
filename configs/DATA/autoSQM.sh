0fd1d29680a751a008bb23c636596c0e2334c482
Eliminater74 <eliminater74@gmail.com>
2022-06-12 11:47:00 -0400

    autoSQM: update fixes

--- configs/DATA/autoSQM.sh
+++ configs/DATA/autoSQM.sh
@@ -11,21 +11,21 @@
  cat /tmp/speedtestResult/speedtestLog-"$(date +"%Y-%m-%d").log" | grep -i upload | tr -d "Upload: " | tr -d Mbps | sort -n > /tmp/upload.txt
  
  #get average speed for download & upload
- download=`cat /tmp/download.txt  | awk -f median.awk`
- upload=`cat /tmp/upload.txt  | awk -f median.awk`
+ download=$(cat /tmp/download.txt  | awk -f median.awk)
+ upload=$(cat /tmp/upload.txt  | awk -f median.awk)
  
  #convert to Kbps & adjust to 90% speed
  downloadKbps=$(awk "BEGIN {download = $download; print download*1000*90/100}")
  uploadKbps=$(awk "BEGIN {upload = $upload; print upload*1000*90/100}")
- echo $downloadKbps
- echo $uploadKbps
+ echo "$downloadKbps"
+ echo "$uploadKbps"
  
  #set value to SQM use uci
  #change value after -gt to only apply SQM if the result greate than X (in kbps)
  if [ "$downloadKbps" -gt 10000 ] && [ "$uploadKbps" -gt 10000 ]; then
  echo "setting up SQM with new value"
- uci set sqm.eth1.upload=${uploadKbps%%.*}
- uci set sqm.eth1.download=${downloadKbps%%.*}
+ uci set sqm.eth1.upload="${uploadKbps%%.*}"
+ uci set sqm.eth1.download="${downloadKbps%%.*}"
  uci commit sqm
  /etc/init.d/sqm reload
  echo "auto sqm DONE!"
