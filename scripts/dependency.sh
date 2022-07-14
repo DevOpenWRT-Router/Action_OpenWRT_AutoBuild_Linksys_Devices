218ae30d3637c8f1e3dbf063b54d0d33ed09ac46
Eliminater74 <eliminater74@gmail.com>
2022-07-13 22:35:19 -0400

    dep: Some more Small changes
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- scripts/dependency.sh
+++ scripts/dependency.sh
@@ -45,65 +45,98 @@ sudo -E apt-get -qq install libxml2-utils make zip patchelf minicom openjdk-8-jd
 echo "::endgroup::"
 
 # uboot v2016
+echo "::group::Installing for uboot v2016"
 sudo -E apt-get -qq install device-tree-compiler
+echo "::endgroup::"
 
 # kernel release-4.4
+echo "::group::Installing for kernel release-4.4"
 sudo -E apt-get -qq install liblz4-tool
 sudo -E apt-get -qq install bison
+echo "::endgroup::"
 
 # recommended
+echo "::group::Installing recommended packages"
 sudo -E apt-get -qq install openssh-server vim
 sudo -E apt-get -qq install qemu-user-static
 sudo -E apt-get -qq install exfat-fuse exfat-utils p7zip-full tree
+echo "::endgroup::"
 
 # build git-2.18+
+echo "::group::Installing for build git-2.18+"
 sudo -E apt-get -qq install autoconf
 sudo -E apt-get -qq install libcurl4-openssl-dev libssh-dev
+echo "::endgroup::"
 
 # build x86-x64 kernel
+echo "::group::Installing for build x86-x64 kernel"
 sudo -E apt-get -qq install pkg-config
 sudo -E apt-get -qq install libelf-dev
+echo "::endgroup::"
 
 # build mtd-utils v2.0.2+
+echo "::group::Installing for build mtd-utils v2.0.2+"
 sudo -E apt-get -qq install libtool
+echo "::endgroup::"
 
 # virtualbox
+echo "::group::Installing for virtualbox"
 sudo -E apt-get -qq install libqt5core5a libqt5gui5 libqt5opengl5 \
 	libqt5printsupport5 libqt5widgets5 libqt5x11extras5 libsdl1.2debian
+  echo "::endgroup::"
 
 # buildroot (rockchip)
+echo "::group::Installing for buildroot (rockchip)"
 sudo -E apt-get -qq install texinfo
 sudo -E apt-get -qq install genext2fs
+echo "::endgroup::"
 
 # crosstool-ng
+echo "::group::Installing for crosstool-ng"
 sudo -E apt-get -qq install lzip help2man libtool libtool-bin
+echo "::endgroup::"
 
 # qemu
+echo "::group::Installing for qemu"
 sudo -E apt-get -qq install debootstrap
+echo "::endgroup::"
 
 # for allwinner
+echo "::group::Installing for allwinner"
 sudo -E apt-get -qq install u-boot-tools swig python-dev python3-dev
+echo "::endgroup::"
 
 # act-greq
+echo "::group::Installing for act-greq"
 sudo apt -qq install ack-grep
+echo "::endgroup::"
 
 # openwrt
+echo "::group::Installing for openwrt"
 sudo -E apt-get -qq install time gettext java-propose-classpath apt zstd
+echo "::endgroup::"
 
 # simg2img
+echo "::group::Installing for simg2img"
 [ ${UBUNTU_CODENAME} = "bionic" ] && sudo -E apt-get -qq install android-tools-fsutils
 [ ${UBUNTU_CODENAME} = "focal" ] && sudo -E apt-get -qq install android-sdk-libsparse-utils
+echo "::endgroup::"
 
 # libreELEC
+echo "::group::Installing for libreELEC"
 sudo -E apt-get -qq install bc lzop xfonts-utils xfonts-utils xfonts-utils xsltproc libjson-perl
+echo "::endgroup::"
 
 # for openwrt armhf <-- libc6:i386 NO Longer Used
 #sudo -E apt-get -qq install libc6:i386
 
 # for wireguard
+echo "::group::Installing for wireguard"
 sudo -E apt-get -qq install libmnl-dev
+echo "::endgroup::"
 
 echo "::group::Symlink (GCC) Setup"
+echo "Setting up symlinks for GCC"
 [ -f /usr/bin/gcc-8 ] && for i in $(ls /usr/bin/*-8); do sudo -E ln -sf "$i" "${i%%-8*}"; done
 [ -f /usr/bin/gcc-9 ] && for i in $(ls /usr/bin/*-9); do sudo -E ln -sf "$i" "${i%%-9*}"; done
 [ -f /usr/bin/gcc-10 ] && for i in $(ls /usr/bin/*-10); do sudo -E ln -sf "$i" "${i%%-10*}"; done
@@ -112,6 +145,7 @@ echo "::group::Symlink (GCC) Setup"
 echo "::endgroup::"
 
 echo "::group::Running Some Final Settings"
+echo "Some Final Settings added"
 sudo -E apt-get clean -qq
 sudo -E ln -sf /usr/include/asm-generic /usr/include/asm
 sudo timedatectl set-timezone "$TZ"
@@ -121,6 +155,7 @@ echo "::endgroup::"
 
 # Running GIT ENV
 echo "::group::GIT: Setting up Action Bot Email and Name"
+echo "Setting up GIT env"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 echo "::endgroup::"
