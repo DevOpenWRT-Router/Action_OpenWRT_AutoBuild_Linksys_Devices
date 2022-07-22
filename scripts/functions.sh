f24dded750dc4a59c3c9f2276fdfa1d34d348a2e
Eliminater74 <eliminater74@gmail.com>
2022-07-22 11:29:51 -0400

    functions: remove old KMODS section (VERY OLD)
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -350,41 +350,6 @@ fi
   echo "FILE_DATE=_$(date +"%Y%m%d%H%M")" >> "$GITHUB_ENV"
 }
 
-### This is needed for creation of kmods directory
-kernel_version() {
-cd openwrt || return
-find build_dir/ -name .vermagic -exec cat {} \; >VERMAGIC  # Find hash
-find build_dir/ -name "linux-5.*.*" -type d >KERNELVERSION # find kernel version
-kv=$(tail -n +2 KERNELVERSION | sed 's/.*x-//')
-vm=$(head -n 1 VERMAGIC)                                # read kernel hash from file                                     # Get last 7 chars from kernel version
-rm -rf VERMAGIC KERNELVERSION                              # remove both files, Not needed anymore
-cd bin/targets/*/* || return
-echo "TARGET_DIR=$PWD" >>"$GITHUB_ENV"
-# TARGET_DIR=$PWD
-KERNEL_VER=$kv"-"$vm                      # add together to complete
-KMOD_DIR=$kv"-"$vm                        # add together to complete
-echo "KERNEL_VER=$kv"-"$vm" >>"$GITHUB_ENV" # store in get actions
-echo "KMOD_DIR=$kv"-"$vm" >>"$GITHUB_ENV"   # store in get actions
-echo "------------------------------------------------"
-echo "Kernel: $KERNEL_VER" # testing
-echo "DIR: $KMOD_DIR"
-echo "------------------------------------------------"
-echo "$KMOD_DIR" >> "$GITHUB_WORKSPACE"/openwrt/kmod
-cat "$GITHUB_WORKSPACE"/openwrt/kmod
-}
-
-### This is really 2nd part of above kernel version
-package_archive() {
-cd "$GITHUB_WORKSPACE"/openwrt || return
-mkdir -p bin/targets/mvebu/cortexa9/kmods/"$KMOD_DIR"
-rsync '--include=/kmod-*.ipk' '--exclude=*' -va bin/targets/mvebu/cortexa9/packages/ bin/targets/mvebu/cortexa9/kmods/"$KMOD_DIR"/
-make -j32 package/index V=s CONFIG_SIGNED_PACKAGES= PACKAGE_SUBDIRS=bin/targets/mvebu/cortexa9/kmods/"$KMOD_DIR"/
-cd bin/targets/mvebu/cortexa9/kmods/"$KMOD_DIR" || exit
-tar -cvzf kmods_"$KMOD_DIR".tar.gz ./*
-mv kmods_"$KMOD_DIR".tar.gz "$GITHUB_WORKSPACE"/openwrt/bin/targets/mvebu/cortexa9/
-cd "$GITHUB_WORKSPACE"/openwrt || return
-}
-
 CREATE_KMODS() {
 set -x
 
